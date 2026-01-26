f5ab382a4fc6303c02ca5bcb9726fba2f218548a
Purna Acharya <152753500+Purna998@users.noreply.github.com>
2026-01-26 20:39:27 +0545

    Day1 Intro and variables

---
+++ day1/day1_ex2.py
@@ -0,0 +1,19 @@
+#Define variables of different data types
+
+integer_var=10
+float_var=3.14
+string_var="100daysofpython"
+list_var=[1,2,3]
+tuple_var=(4,5,6)
+dict_var={"name":"Alice","role":"CS Engineer"}
+bool_var=True
+
+#print and manipulate variables 
+print("Integer:",integer_var)
+print("Float:",float_var)
+print("String:",string_var+"Bootcamp")
+list_var.append(4)
+print("List:",list_var)
+print("Tuple:",tuple_var)
+print("Dictionary Value:",dict_var['name'])
+print("Boolean:",bool_var)
---
+++ day1/day1_intro.py
@@ -0,0 +1,18 @@
+#Integers and floats
+age=25
+height=5.9
+
+#Strings
+name="Purna"
+greeting="Hello,"+name
+
+#List and tuple
+numbers=[1,2,3,4]
+names=["Alice","Jack"]
+coordinates=(10,20)
+
+#Dictionary
+person={"name":"Purna","age":25}
+
+#Booleans
+is_student=True
